ebf7097aa8ab501f53b70c5bc170e84b67abbede
JefsonOliveira <kaironoliveira16@gmail.com>
2024-08-10 21:50:47 -0300

    feat: Criação do Componente NavBar e estelizações

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import NavBar from "../../components/NavBar/NavBar";
+import Hero from "./sections/Hero/Hero"
+
+const Home = () => {
+  return <div>
+    <NavBar />
+    <Hero />
+  </div>;
+};
+
+export default Home;
